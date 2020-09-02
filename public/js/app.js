a11290ed75c4bfce22caac19b7f9ef88934e0fe6
learnSSR <saurabhrawat371@gmail.com>
2020-09-02 15:42:06 +0530

    set up

--- public/js/app.js
+++ public/js/app.js
@@ -8,7 +8,7 @@ weatherform.addEventListener('submit' , (e) => {
 	e.preventDefault()
 	console.log(search.value)
 	disp.textContent = 'loading...'
-	fetch('http://localhost:3000/weather?search='+search.value).then( (response) => {
+	fetch('/weather?search='+search.value).then( (response) => {
 	response.json().then( (data) => {
 		console.log(data)
 
